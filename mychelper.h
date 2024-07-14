d820959cfcf5d1d7c3eed29b35361d4cf227d52b
Xander Ison <73865197+imxaander@users.noreply.github.com>
2024-07-14 22:54:46 +0800

    Update mychelper.h

--- mychelper.h
+++ mychelper.h
@@ -7,4 +7,9 @@ void swap_int(int* a, int* b){
     *a = *a ^ *b;
 }
 
+void print_int_array_contents(int* a, int b){
+    for(int i = 0; i < b; i++){
+        printf("%i", *(a+i));
+    }
+}
 #endif
